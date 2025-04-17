936dc7cd4d63aec73d822d2f4c0a157a06a745d0
Mukundmishra120 <149421328+Mukundmishra120@users.noreply.github.com>
2025-04-18 02:40:40 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import VideoConfrencing from "./VideoConfrencing.jsx"
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="video">Video Confrencing App</h1>
+      <VideoConfrencing/>
+    </>
+  )
+}
+
+export default App
